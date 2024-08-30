97be1ebce4f3b8255111e535f9d0e98809fd7db2
tuwnguyen <tuwnguyen@gmail.com>
2024-08-30 16:37:47 +0700

    feat: add auth logic

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+import { currentUserRouter } from './router/current-user'
+import { signinRouter } from './router/signin'
+import { signoutRouter } from './router/signout'
+import { signupRouter } from './router/signup'
+
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there');
-});
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signupRouter)
 
 app.listen(3000, () => {
   console.log('listening on port 3000!!!');
