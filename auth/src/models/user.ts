e5a808022e20b3c2a3798672cadb4f7a7992aa15
tuwnguyen <tuwnguyen@gmail.com>
2024-09-02 23:54:48 +0700

    feat: adding UserDocument for create UserModel, adding BadRequestError

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -7,9 +7,16 @@ interface UserAttrs {
 
 
 // An interface that describes the properties that a User Model has
-interface UserModel extends mongoose.Model<any>{
-  build(attrs: UserAttrs): any
+interface UserModel extends mongoose.Model<UserDoc>{
+  build(attrs: UserAttrs): UserDoc
 }
+
+// An interface that describes the properties that a User Document has
+interface UserDoc extends mongoose.Document {
+  email: string
+  password: string
+}
+
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -26,8 +33,8 @@ userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs)
 }
 
-const User = mongoose.model<any, UserModel>('User', userSchema)
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema)
 
-User.build({email: '12312', password: 'sdsd'})
+
 
 export { User }
--- auth/src/router/signup.ts
+++ auth/src/router/signup.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
 import { RequestValidationError } from '../errors/request-validation-error'
 import { DatabaseConnectionError } from '../errors/database-connection-error'
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -19,9 +21,16 @@ router.post('/api/users/signup', [
     throw new RequestValidationError(errors.array())
 
   const { email, password } = req.body
-  console.log('Creating a user...')
-  throw new DatabaseConnectionError()
-  res.send({})
+
+  const existingUser = await User.findOne({ email })
+  if (existingUser) {
+    throw new BadRequestError('Email in use')
+  }
+  
+  const user = User.build({ email, password })
+  await user.save()
+
+  res.status(201).send(user)
   
 });
 
