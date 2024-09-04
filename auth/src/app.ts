7f3de97af21f87c053d2d2c50106ec8173157ff0
tuwnguyen <tuwnguyen@gmail.com>
2024-09-04 22:26:04 +0700

    feat: testing: create app.ts, add few dependencies for testing, edit Dockerfile to omit devDependency

---
+++ auth/src/app.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+// https://www.npmjs.com/package/express-async-errors
+// Error handling async error
+import 'express-async-errors'
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './router/current-user'
+import { signinRouter } from './router/signin'
+import { signoutRouter } from './router/signout'
+import { signupRouter } from './router/signup'
+import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
+
+app.use(json());
+
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signupRouter)
+
+app.all('*', async () => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export { app }
