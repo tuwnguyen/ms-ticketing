813b302c6b9941975bdf10dd38c8612e58c17028
tuwnguyen <tuwnguyen@gmail.com>
2024-09-06 00:32:45 +0700

    chore: config server-side access auth srv in Landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,16 @@
-const Landing = () => {
-  return <h1>Landing page</h1>
-}
+import buildClient from "../api/build-client";
 
-export default Landing
+const Landing = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signd in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
+};
+
+Landing.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+  return data;
+};
+export default Landing;
