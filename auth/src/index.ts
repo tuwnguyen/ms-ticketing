156a7b15c48b3eabd2f82b99ec68ca9fb2f39130
tuwnguyen <tuwnguyen@gmail.com>
2024-09-07 12:55:59 +0700

    chore: update MONGO_URI env var for auth and tickets

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,22 +1,24 @@
-import mongoose from 'mongoose'
-import { app } from './app'
+import mongoose from "mongoose";
+import { app } from "./app";
 
-const start = async() => {
+const start = async () => {
   if (!process.env.JWT_KEY) {
-    throw new Error('JWT must be defined')
+    throw new Error("JWT must be defined");
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
   }
 
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
-    console.log('Connected to MongoDB')
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 
   app.listen(3000, () => {
-    console.log('listening on port 3000!!!');
+    console.log("listening on port 3000!!!");
   });
-}
+};
 
-start()
-
+start();
