bcf0a5c4dd1fdbb8d3dd2ffc11f566a340def6f2
tuwnguyen <tuwnguyen@gmail.com>
2024-09-09 17:38:18 +0700

    chore: test receive message in listener and config QUEUE GROUP in service

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,16 +1,23 @@
-import nats from "node-nats-streaming";
-
+import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
 console.clear();
 
-const stan = nats.connect("ticketing", "123", {
+const clientID = randomBytes(4).toString("hex");
+const stan = nats.connect("ticketing", clientID, {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("Event Listened");
 
-  const subscription = stan.subscribe("ticket:created");
-  subscription.on("message", () => {
-    console.log(`Received message`);
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "order-service-queue-group"
+  );
+  subscription.on("message", (msg: Message) => {
+    const data = msg.getData();
+    if (typeof data === "string") {
+      console.log(`Received event #${msg.getSequence()}, with data ${data}}`);
+    }
   });
 });
