6d84e732c2087251a4dd35bdba5461b5764b7802
tuwnguyen <tuwnguyen@gmail.com>
2024-09-10 16:52:59 +0700

    feat: Add subscription options (deliverAllAvailable, durable name) for NATS listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -15,7 +15,12 @@ stan.on("connect", () => {
     process.exit();
   });
 
-  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName("accounting-service");
+
   const subscription = stan.subscribe(
     "ticket:created",
     "order-service-queue-group",
