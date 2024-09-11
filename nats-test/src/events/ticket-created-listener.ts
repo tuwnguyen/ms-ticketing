9ab8dd8b5d30a54c2cdb849864a60b12b8cf6f3c
tuwnguyen <tuwnguyen@gmail.com>
2024-09-11 14:43:49 +0700

    refactor: custom event interface and enforcing Listener Subjects and Data

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,11 +1,13 @@
 import { Message } from "node-nats-streaming";
 import { Listener } from "./base-listener";
+import { TicketCreatedEvent } from "./ticket-created-event";
+import { Subjects } from "./subjects";
 
-export class TicketCreatedListener extends Listener {
-  subject = "ticket:created";
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
   queueGroupName = "payment-service";
 
-  onMessage(data: any, msg: Message): void {
+  onMessage(data: TicketCreatedEvent["data"], msg: Message): void {
     console.log("Event data!", data);
 
     msg.ack();
