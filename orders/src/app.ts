60f4d1b23d42f0a1f40c367278dc3605f48745b9
tuwnguyen <tuwnguyen@gmail.com>
2024-09-12 19:07:27 +0700

    feat: Added some routes in Orders service

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -5,10 +5,10 @@ import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { currentUser, errorHandler, NotFoundError } from "@tuwtickets/common";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes";
-import { updateTicketRouter } from "./routes/update";
+import { indexOrderRouter } from "./routes";
+import { deleteOrderRouter } from "./routes/delete";
+import { showOrderRouter } from "./routes/show";
+import { newOrderRouter } from "./routes/new";
 
 const app = express();
 app.set("trust proxy", true);
@@ -23,10 +23,10 @@ app.use(
 
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
+app.use(showOrderRouter);
+app.use(newOrderRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
