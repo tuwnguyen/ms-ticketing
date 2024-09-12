03a31149ed719c3f6def174d73e75fc594acb449
tuwnguyen <tuwnguyen@gmail.com>
2024-09-12 15:40:45 +0700

    refactor: Ensure NATS client is initialized before access by adding a check in the client getter - Graceful shutdown NATS in index.ts

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -13,6 +13,16 @@ const start = async () => {
 
   try {
     await natsWrapper.connect("ticketing", "asdasd", "http://nats-srv:4222");
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    // INTERRUPT SIGNAL
+    process.on("SIGINT", () => natsWrapper.client.close());
+
+    // TERMINATE SIGNAL
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
