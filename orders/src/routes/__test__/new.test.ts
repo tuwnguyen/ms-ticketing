017701f21a84a8fe8032c73af91f569e9702b77e
tuwnguyen <tuwnguyen@gmail.com>
2024-09-13 12:48:10 +0700

    test: Write emits event in delete and new Orders service

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -4,6 +4,8 @@ import { app } from "../../app";
 import { Order, OrderStatus } from "../../models/order";
 import { Ticket } from "../../models/ticket";
 
+import { natsWrapper } from "../../nats-wrapper";
+
 it("returns an error if the ticket doesnt exist", async () => {
   const ticketId = new mongoose.Types.ObjectId();
 
@@ -50,4 +52,17 @@ it("reserves a ticket", async () => {
     .expect(201);
 });
 
-it.todo("emits an order created event");
+it("emits an order created event", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 10,
+  });
+  await ticket.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
