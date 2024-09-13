10975bb5b2b4f281f192f5df6fc64868e1d9d74c
tuwnguyen <tuwnguyen@gmail.com>
2024-09-14 00:03:43 +0700

    refactor: abstract query event method

--- orders/src/events/listener/ticket-updated-listener.ts
+++ orders/src/events/listener/ticket-updated-listener.ts
@@ -7,8 +7,8 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   readonly subject = Subjects.TicketUpdated;
   queueGroupName = queueGroupName;
   async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
-    const { id, title, price, userId } = data;
-    const ticket = await Ticket.findById(id);
+    const { title, price } = data;
+    const ticket = await Ticket.findByEvent(data);
 
     if (!ticket) throw new Error("Ticket not found");
 
--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -10,6 +10,10 @@ interface TicketAttrs {
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
+  findByEvent(event: {
+    id: string;
+    version: number;
+  }): Promise<TicketDoc | null>;
 }
 
 export interface TicketDoc extends mongoose.Document {
@@ -43,6 +47,10 @@ const ticketSchema = new mongoose.Schema(
 
 ticketSchema.set("versionKey", "version");
 ticketSchema.plugin(updateIfCurrentPlugin);
+
+ticketSchema.statics.findByEvent = (event: { id: string; version: number }) => {
+  return Ticket.findOne({ _id: event.id, version: event.version - 1 });
+};
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket({
