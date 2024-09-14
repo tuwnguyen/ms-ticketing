ea736fc7910428974ff45d7ba67901d46b46fbf5
tuwnguyen <tuwnguyen@gmail.com>
2024-09-14 12:17:54 +0700

    optional: config alternative way to customize the update-if-current plugin

--- orders/src/events/listener/ticket-updated-listener.ts
+++ orders/src/events/listener/ticket-updated-listener.ts
@@ -7,12 +7,12 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   readonly subject = Subjects.TicketUpdated;
   queueGroupName = queueGroupName;
   async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
-    const { title, price } = data;
+    const { title, price, version } = data;
     const ticket = await Ticket.findByEvent(data);
 
     if (!ticket) throw new Error("Ticket not found");
 
-    ticket.set({ title, price });
+    ticket.set({ title, price, version });
     await ticket.save();
 
     msg.ack();
