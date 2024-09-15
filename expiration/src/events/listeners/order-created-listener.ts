660bb05a03e8f558a1fb73c88f7409c0d71c2a8c
tuwnguyen <tuwnguyen@gmail.com>
2024-09-15 11:39:41 +0700

    feat: Added delay time based on expiration time to the job

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -7,7 +7,10 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
-    await expirationQueue.add({ orderId: data.id });
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log("Waiting this many miliseconds to process the job", delay);
+
+    await expirationQueue.add({ orderId: data.id }, { delay });
 
     msg.ack();
   }
