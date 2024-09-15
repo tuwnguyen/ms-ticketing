fa755d4e6cbe11e96e54a91e471b9965cbb9bf6a
tuwnguyen <tuwnguyen@gmail.com>
2024-09-16 00:51:52 +0700

    feat: create Payment model to save charge

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -10,6 +10,7 @@ import {
 } from "@tuwtickets/common";
 import { Order } from "../models/order";
 import { stripe } from "../stripe";
+import { Payment } from "../models/payment";
 
 const router = express.Router();
 
@@ -30,12 +31,17 @@ router.post(
     if (order.status === OrderStatus.Cancelled)
       throw new BadRequestError("Cannot pay for an cancelled order");
 
-    // TODO: do payment
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
       currency: "usd",
       amount: order.price * 100, // calculate by cent
       source: token,
     });
+
+    const payment = Payment.build({
+      orderId,
+      stripeId: charge.id,
+    });
+    await payment.save();
 
     res.status(201).send({ success: true });
   }
