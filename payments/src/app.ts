5889d02329d00d4c6afc938e9c53202b348e1986
tuwnguyen <tuwnguyen@gmail.com>
2024-09-15 13:58:22 +0700

    feat: setup Payment service

---
+++ payments/src/app.ts
@@ -0,0 +1,28 @@
+import express from "express";
+// https://www.npmjs.com/package/express-async-errors
+// Error handling async error
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { currentUser, errorHandler, NotFoundError } from "@tuwtickets/common";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    // making a request to our server over an HTTPS connection
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+app.use(currentUser);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import jwt from "jsonwebtoken";
+declare global {
+  var signin: () => string[];
+}
+
+jest.mock("../nats-wrapper");
+
+let mongod: MongoMemoryServer;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = "asdasd";
+  mongod = await MongoMemoryServer.create();
+  const mongoUri = mongod.getUri();
+  await mongoose.connect(mongoUri);
+
+  mongoose.connection.on("error", (error) => {
+    console.error("MongoDB connection error:", error);
+  });
+
+  mongoose.connection.once("open", () => {
+    console.log("Connected to MongoDB Memory Server");
+  });
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db?.collections();
+  if (collections) {
+    for (let collection of collections) {
+      await collection.deleteMany({});
+    }
+  }
+});
+
+afterAll(async () => {
+  if (mongod) {
+    await mongod.stop();
+  }
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "test@test.com",
+  };
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  const sessionJSON = JSON.stringify({ jwt: token });
+
+  // const base64 = btoa(session);
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  const cookie = `session=${base64}`;
+
+  return [cookie];
+};
