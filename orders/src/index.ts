aa6952cf4abf7daac099ac17e13a7052abf5e227
tuwnguyen <tuwnguyen@gmail.com>
2024-09-15 11:55:20 +0700

    refactor: edit listener path

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
-import { TicketCreatedListener } from "./events/listener/ticket-created-listener";
-import { TicketUpdatedListener } from "./events/listener/ticket-updated-listener";
+import { TicketCreatedListener } from "./events/listeners/ticket-created-listener";
+import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
