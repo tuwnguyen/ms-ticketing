522af520316c8c324e76e90015a93b0505432505
tuwnguyen <tuwnguyen@gmail.com>
2024-09-19 18:10:53 +0700

    refactor: Added ConfigMap infra to use BASE_URL in Client srv

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,8 +4,7 @@ export default ({ req }) => {
   if (typeof window === "undefined") {
     // we are on the server
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: process.env.BASE_URL,
       headers: req.headers,
     });
   } else {
